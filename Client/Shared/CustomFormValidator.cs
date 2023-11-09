6bc0079a4324687b1733860f1019433874f1e09b
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2023-11-09 18:23:24 +0530

    Update the project

---
+++ Client/Shared/CustomFormValidator.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+
+namespace BlazorFormsValidation.Client.Shared
+{
+    public class CustomFormValidator : ComponentBase
+    {
+        private ValidationMessageStore validationMessageStore;
+
+        [CascadingParameter]
+        private EditContext CurrentEditContext { get; set; }
+
+        protected override void OnInitialized()
+        {
+            if (CurrentEditContext == null)
+            {
+                throw new InvalidOperationException(
+                    $"{nameof(CustomFormValidator)} requires a cascading parameter of type {nameof(EditContext)}.");
+            }
+
+            validationMessageStore = new ValidationMessageStore(CurrentEditContext);
+
+            CurrentEditContext.OnValidationRequested += (s, e) =>
+                validationMessageStore.Clear();
+            CurrentEditContext.OnFieldChanged += (s, e) =>
+                validationMessageStore.Clear(e.FieldIdentifier);
+        }
+
+        public void DisplayFormErrors(Dictionary<string, List<string>> errors)
+        {
+            foreach (var err in errors)
+            {
+                validationMessageStore.Add(CurrentEditContext.Field(err.Key), err.Value);
+            }
+
+            CurrentEditContext.NotifyValidationStateChanged();
+        }
+
+        public void ClearFormErrors()
+        {
+            validationMessageStore.Clear();
+            CurrentEditContext.NotifyValidationStateChanged();
+        }
+    }
+}
---
+++ Shared/Models/StudentRegistration.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorFormsValidation.Shared.Models
+{
+    public class StudentRegistration
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [UserNameValidation]
+        public string Username { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$",
+            ErrorMessage = "Password should have minimum 8 characters, at least 1 uppercase letter, 1 lowercase letter and 1 number.")]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "The password and confirm password fields do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+    }
+}
